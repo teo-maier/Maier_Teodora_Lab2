e74c35d6eb5c4f5bd25e2e158a536c0187141b79
Teo Maier <teo.maier@ontegrasolutions.com>
2022-11-09 22:06:22 +0200

    Rest of files

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,7 @@
+﻿namespace Maier_Teodora_Lab2.Models.ViewModels;
+
+public class PublisherIndexData
+{
+    public IEnumerable<Publisher> Publishers { get; set; }
+    public IEnumerable<Book> Books { get; set; }
+}
